4eafecaadcb1d108af379ec34bf9e96c250a8dab
weitunglin <weitung8@gmail.com>
2021-06-13 16:40:36 +0800

    init kinect project

---
+++ KinectGame/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectGame
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor sensor = null;
+        private FrameDescription frameDescription = null;
+        private WriteableBitmap bitmap = null;
+
+        private ColorFrameReader colorFrameReader = null;
+
+        private BodyFrameReader bodyFrameReader = null;
+
+        private Body[] bodies = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.sensor = KinectSensor.GetDefault();
+            this.frameDescription = this.sensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Rgba);
+
+            this.colorFrameReader = this.sensor.ColorFrameSource.OpenReader();
+            this.colorFrameReader.FrameArrived += ColorFrameReader_FrameArrived;
+
+            this.bitmap = new WriteableBitmap(this.frameDescription.Width, this.frameDescription.Height, 96, 96, PixelFormats.Bgr32, null);
+            this.sensor.Open();
+        }
+
+        private void ColorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame == null)
+                    return;
+
+                using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
+                {
+                    this.bitmap.Lock();
+
+                    if (this.frameDescription.Width == this.bitmap.PixelWidth && this.frameDescription.Height == this.bitmap.PixelHeight)
+                    {
+                        colorFrame.CopyConvertedFrameDataToIntPtr(
+                            this.bitmap.BackBuffer,
+                            (uint)(this.frameDescription.Width * this.frameDescription.Height * 4),
+                            ColorImageFormat.Bgra);
+
+                        this.bitmap.AddDirtyRect(new Int32Rect(0, 0, this.bitmap.PixelWidth, this.bitmap.PixelHeight));
+                    }
+
+                    this.bitmap.Unlock();
+                }
+            }
+        }
+
+        private void Kinect_Class2_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ImageSource.Source = this.bitmap;
+        }
+
+        private void Kinect_Class2_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (this.colorFrameReader != null)
+            {
+                this.colorFrameReader.Dispose();
+                this.colorFrameReader = null;
+            }
+
+            if (this.sensor != null)
+            {
+                this.sensor.Close();
+                this.sensor = null;
+            }
+        }
+    }
+}
